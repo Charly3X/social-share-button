b9b38f51bbf1d6b3f1a73dfbb0151d49873d63b2
charoian <kinovhod@gmail.com>
2016-04-29 17:02:21 +0300

    add new buttons

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
+#ToDo: Need refactoring
 module SocialShareButton
   module Helper
     def social_share_button_tag(title = "", opts = {})
@@ -39,13 +40,57 @@ module SocialShareButton
     end
 
 
+    def social_share_button_window_tag(title = "", opts = {})
+      html = []
+      html << "<ul class='ov_share_list social-share-button' data-title='#{h title}' data-img='#{opts[:image]}' data-url='#{opts[:url]}' data-desc='#{opts[:desc]}' data-popup='#{opts[:popup]}' data-via='#{opts[:via]}'>"
+
+      SocialShareButton.config.allow_sites.each do |name|
+        name_class = name
+        name_social = name
+        shares = 'shares'
+        if name == 'twitter'
+          shares = nil
+        end
+        if name == 'google_plus'
+          name_class = 'google'
+          name_social = 'Google+'
+        end
+
+        html << "<li class='ov_share_item'>"
+        html << "<a class='ov_share_link #{name_class}_mod' href='#' data-site='#{name}' onclick='return SocialShareButton.share(this);' >
+                   <dl class='ov_share_dl'>
+                     <dt class='ov_share_dt'>#{name_social.capitalize}</dt>
+                     <dd class='ov_share_dd'>#{get_count(name, opts[:url])} #{shares}</dd>
+                     <dd class='ov_share_dd visual_mod'>"
+        if name == 'facebook'
+          html <<  "<span class='fb_mod mod_0 ov_share_visual'></span>"
+        end
+        if name == 'twitter'
+          html <<  "<span class='tw_mod mod_0 ov_share_visual'></span>"
+        end
+        if name == 'google_plus'
+          html <<  "<span class='gl_mod mod_0 ov_share_visual'></span>"
+        end
+        if name == 'delicious'
+          html <<  "<img class='share_img' src='#{image_path('delicious_logo.jpg')}'>"
+        end
+        if name == 'reddit'
+          html <<  "<img class='share_img' src='#{image_path('reddit_logo.png')}'>"
+        end
+        html << "</dd></dl></a>"
+        html << "</li>"
+      end
+      html << "</ul>"
+      raw html.join("\n")
+    end
+
     def social_share_button_left_tag(title = "", opts = {})
       html = []
 
       html << "<div class='likes_block'>
            <ul class='likes_list' data-url='#{opts[:url]}' data-popup='#{opts[:popup]}'>
               <li class='likes_item'>
-                 <a class='likes_link mod_share' href='#' title='#'><span class='title_likes mod_share'>Share</span></a>
+                 <a class='likes_link mod_share js_ov_butt_share' href='#' title='#'><span class='title_likes mod_share'>Share</span></a>
               </li>
               <li class='likes_item'>
                  <a class='likes_link mod_fb facebook' data-site='facebook' href='#' onclick='return SocialShareButton.share(this);' title='#'><span class='title_likes mod_fb facebook'>Like</span></a>
